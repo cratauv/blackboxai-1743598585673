b08c6a1e37b3e41090351c1d4e055372cb88a761
cratauv <81857838+cratauv@users.noreply.github.com>
2025-04-02 15:57:05 +0300

    Add client/src/app/booking-options/booking-options.service.ts 1743598624443

---
+++ client/src/app/booking-options/booking-options.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface BookingOption {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  duration: number;
+  category: string;
+  imageUrl: string;
+  rating: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class BookingOptionsService {
+  private http = inject(HttpClient);
+
+  searchOptions(query: string, filters: any = {}): Observable<BookingOption[]> {
+    return this.http.get<BookingOption[]>('/api/booking-options/search', {
+      params: { q: query, ...filters }
+    });
+  }
+
+  getFeaturedOptions(): Observable<BookingOption[]> {
+    return this.http.get<BookingOption[]>('/api/booking-options/featured');
+  }
+
+  getOptionDetails(id: string): Observable<BookingOption> {
+    return this.http.get<BookingOption>(`/api/booking-options/${id}`);
+  }
+}

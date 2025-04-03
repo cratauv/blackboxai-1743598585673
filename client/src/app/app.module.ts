296b4f595d8d72a2d197237028798d9301d832c0
cratauv <81857838+cratauv@users.noreply.github.com>
2025-04-03 09:02:00 +0300

    Add client/src/app/app.module.ts 1743660120387

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookingOptionsModule } from './booking-options/booking-options.module';
+import { EmailNotificationsModule } from './email-notifications/email-notifications-new.module';
+import { PaymentProcessingComponent } from './payment/payment-processing.component';
+import { AnalyticsDashboardComponent } from './analytics/analytics-dashboard-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalyticsDashboardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BookingOptionsModule
+    BookingOptionsModule,
+    EmailNotificationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

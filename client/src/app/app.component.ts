4a1f2dfb67913ed630d956292b0e0b0a7543cdf0
cratauv <81857838+cratauv@users.noreply.github.com>
2025-04-05 17:03:05 +0300

    Add client/src/app/app.component.ts 1743861785097

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslationService } from './localization/services/translation.service';
 import { NotificationCenterComponent } from './in-app-notifications/notification-center.component';
 import { NotificationService } from './in-app-notifications/notification.service';
 import { AnalyticsDashboardComponent } from './analytics/analytics-dashboard-new.component';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="container">
-      <h1>Booking Options Reviews</h1>
-      <router-outlet></router-outlet>
-    </div>
-  `,
-  styles: [`
-    .container {
-      max-width: 1200px;
-      margin: 0 auto;
-      padding: 20px;
-    }
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private translationService: TranslationService) {}
+
+  ngOnInit() {
+    this.translationService.loadTranslations('en'); // Load default language
+  }
+}
